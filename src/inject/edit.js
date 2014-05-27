bd3d7108158a354e47c13f035962de21de50117d
Kaustubh <theTuxRacer@gmail.com>
2014-05-27 12:31:06 +0530

    KP | Regex that replaces _all_ occurences.

---
+++ src/inject/edit.js
@@ -0,0 +1,3 @@
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Bieber", "g"), "Butt");
+// document.body.innerHTML = document.body.innerHTML.replace(new RegExp("BIEBER", "g"), "BUTT");
+// document.body.innerHTML = document.body.innerHTML.replace(new RegExp("bieber", "g"), "butt");
